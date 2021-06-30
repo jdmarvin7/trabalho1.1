40d13894e9ed77b50a85ac678ccd86661f2ce202
jdmarvin7 <jdmarvinmaeldestin02@gmail.com>
2021-06-30 10:04:48 -0300

    Volte ao normal

--- exercicio1.py
+++ exercicio1.py
@@ -1,5 +1,5 @@
 #Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica:
-# utilize o operador módulo (resto da divisão)
+# utilize o operador módulo (resto da divisão).
 
 numero = int(input('digite um numero: '))
 if numero%2 == 0:
